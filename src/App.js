d6748c2796099215ba97a9ffeac934d324d4674b
alexbponder@gmail.com <alexbponder@gmail.com>
2021-12-02 17:26:59 -0800

    added query

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import axios from 'axios';
+import React, {
+  useState,
+  useEffect
+} from 'react';
+
 
 function App() {
+  const [testdata, settestdata] = useState([]);
+  useEffect(()=> {
+    async function fetchdata(){
+      const response = await axios.get('/api')
+      console.log(response)
+      settestdata(response.data)
+    }
+    fetchdata()
+  },[])
   return (
     <div>
-      Hello World
+      {testdata.map((obj, idx) => (
+        <div>{obj.ProvName}</div>
+      ))}
     </div>
   );
 }
