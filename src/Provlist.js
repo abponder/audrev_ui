ed0012fa4e111636b59aabb8b5a5a2f4c1ea5913
alexbponder@gmail.com <alexbponder@gmail.com>
2022-03-08 17:37:23 -0800

    added label etc

--- src/Provlist.js
+++ src/Provlist.js
@@ -6,23 +6,51 @@ import React, {
   useEffect
 } from 'react';
 
-
+// https://codebrahma.com/build-table-componenet-with-react-hooks/
 
 function Provlist() {
   const [provdata, setprovdata] = useState([]);
+  const [tblheadings, settblheadings] = useState([]);
   useEffect(()=> {
     async function fetchdata(){
       const response = await axios.get('/api/provlist')
-      console.log(response)
+      //console.log(response)
+      settblheadings(Object.keys(response.data[0]))
       setprovdata(response.data)
     }
     fetchdata()
   },[]) 
+
+
   return (
     <div>
-      {provdata.map((obj, idx) => (
-        <div key={idx}>{obj.MedCtr}</div>
+      <br></br>
+      <h3>NEW PROVIDER TRAINING BY MEDICAL CENTER</h3>
+      <br></br>
+      <table>
+      <thead>
+        <tr>{tblheadings.map((key, index) => {
+        return <th key={index}>{key.toUpperCase()}</th>
+    })}</tr>
+      </thead>
+    <tbody>
+    {provdata.map((obj, idx) => (
+        <tr>
+          <td >{obj.MedCtr}</td>
+          <td >{obj.MedOffice}</td>
+          <td >{obj.ProvName}</td>
+          <td >{obj.ProvRole}</td>
+          <td >{obj.Specialty}</td> 
+          <td >{obj.HireDate}</td>
+          <td>{obj.CPMID}</td>
+          <td >{obj.NUID}</td>
+          <td >{obj.OverallStatus}</td>
+          <td >{obj.OverallStatusDate}</td>
+      </tr>
       ))}
+    </tbody>
+</table>
+     
     </div>
   );
 }
