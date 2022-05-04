e2ad71a1d2c8e21495e6540730d1e53485347141
alexbponder@gmail.com <alexbponder@gmail.com>
2022-05-03 18:41:03 -0700

    added status drop down

--- src/Provmed2.js
+++ src/Provmed2.js
@@ -15,14 +15,19 @@ function Provmed2() {
   const location = useLocation();
   const [showform, setshowform] = useState(false)
   const [currentformdata, setcurrentformdata] = useState(null)
+  const [inputs, setInputs] = useState({});
+  const [statuses, setstatuses] = useState([]);
   // console.log('test')
   console.log(location.state)
   useEffect(()=> {
     async function fetchdata2(){
       const response = await axios.get(`/api/provmed2/${location.state.ID_newprov}`)
+      const statusdrpdwn = await axios.get(`/api/provmed3Status`)
+      console.log(statusdrpdwn)
       console.log((response.data))
       settblheadings(Object.keys(response.data[0]))
       setprovdata(response.data)
+      setstatuses(statusdrpdwn.data)
     }
     fetchdata2()
   },[]) 
@@ -32,6 +37,19 @@ function Provmed2() {
     console.log("clicked", rowData)
     setshowform(true)
     setcurrentformdata(rowData)
+    setInputs(rowData)
+  }
+
+  const handleSubmit = (e, topic) => {
+    
+    e.preventDefault()
+    console.log(inputs)
+  }
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInputs(values => ({...values, [name]: value}))
   }
 
   return (
@@ -40,12 +58,77 @@ function Provmed2() {
       <h3>NEW PROVIDER TRAINING BY PHASE</h3>
       <h4>PROVIDER: {location.state.provider}</h4>
       <h4>Medical Center: {location.state.MOB}</h4>
+      
       <br></br>
       {showform && (
-        <p>this is form spot {currentformdata.comments}</p>
-        // <form>
+        <div>
+        <h4>Phase: {currentformdata.phase}</h4>
+        {/* // ID_newprov: 1
+        // ID_phase: 2
+        // MedOffice: "Darpa"
+        // ProvName: "O. K. Roberts"
+        // comments: "done with phase 2!"
+        // phase: 2
+        // reviewer: "Lisa Te"
+        // status: "Completed"
+        // status_date: "2022-01-15T08:00:00.000Z" */}
+        <form onSubmit={handleSubmit}>
+        <label>Comments:
+        <br />
+        <input 
+          type="text" 
+          name="comments" 
+          value={inputs.comments} 
+          onChange={handleChange}
 
-        // </form>
+        />
+        </label>
+        <br />
+        <label for="status">Phase Status:</label>
+        <br />
+        <select name='status' onChange={handleChange} value={inputs.status}>
+          {statuses.map(status =>(
+            <option value={status.statusDesc}>{status.statusDesc}</option>
+          ))}
+          <br />
+        </select>
+        {/* <label>Status:
+        <br />
+        <input 
+          type="text" 
+          name="status" 
+          value={inputs.status} 
+          onChange={handleChange}
+        />
+        </label> */}
+        <br />
+        <label>Status Date:
+        <br />
+        <input 
+          type="text" 
+          name="status_date" 
+          value={currentformdata.status_date} 
+          disabled
+        />
+        </label>
+        <br />
+        <label>Reviewer:
+        <br />
+        <input 
+          type="text" 
+          name="reviewer" 
+          value={inputs.reviewer} 
+          onChange={handleChange}
+        />
+        </label>
+        <br />
+        <br />
+          
+          <input type="button" value="Cancel" onClick={()=> setshowform(false)} />
+          <input type="submit" />
+
+      </form>
+      </div>
       )}
       {!showform && <table>
       <thead>
